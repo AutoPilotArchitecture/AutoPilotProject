e61f075f185fca74ff784a5fa3dc000273343814
dikshagunnewar <dikshagunnewar@DESKTOP-OFV4UAH>
2017-09-11 13:39:31 +0530

    changed account saetting

---
+++ Autopilot/EntitiesLayer/Entities/AccessDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer.Entities
+{
+    /// <summary>
+    /// data
+    /// </summary>
+    public class AccessDetails
+    {
+        public int Id { get; set; }
+        public string AccessToken { get; set; }
+        public string AccessTokenSecret { get; set; }
+        [Column(TypeName = "DateTime2")]
+        public DateTime UpdateDateTime { get; set; }
+        [Column(TypeName = "DateTime2")]
+        public DateTime Expires_in { get; set; }
+        public string Refresh_token { get; set; }
+    }
+}
